917e45da7cc2c123c05a0634b2a3e8ac34c5720b
yong <xxy668@foxmail.com>
2018-04-26 22:16:11 +0800

    reverse stack code finish

--- src/main/java/com/henryxi/algorithm/stack/reverse/ReverseStackClient.java
+++ src/main/java/com/henryxi/algorithm/stack/reverse/ReverseStackClient.java
@@ -1,7 +1,38 @@
 package com.henryxi.algorithm.stack.reverse;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class ReverseStackClient {
     public static void main(String[] args) {
-        //test git email
+        Random random = new Random();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 10; i++) {
+            stack.push(random.nextInt(10));
+        }
+        System.out.println("current stack:" + stack);
+        System.out.println("reverse");
+        reverse(stack);
+        System.out.println("current stack:" + stack);
+    }
+
+    public static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int last = popBottom(stack);
+        reverse(stack);
+        stack.push(last);
+    }
+
+    public static int popBottom(Stack<Integer> stack) {
+        Integer top = stack.pop();
+        if (stack.isEmpty()) {
+            return top;
+        } else {
+            int last = popBottom(stack);
+            stack.push(top);
+            return last;
+        }
     }
 }

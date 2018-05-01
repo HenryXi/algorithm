3243d097222490603cf5a26e3a8a9274033795bf
yong <xxy668@foxmail.com>
2018-05-01 21:30:50 +0800

    sort stack code not finish

---
+++ src/main/java/com/henryxi/algorithm/stack/sortstack/SortStackClient.java
@@ -0,0 +1,48 @@
+package com.henryxi.algorithm.stack.sortstack;
+
+import java.util.Random;
+import java.util.Stack;
+
+public class SortStackClient {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Stack<Integer> originalStack = new Stack<>();
+        for (int i = 0; i < 10; i++) {
+            originalStack.push(random.nextInt(random.nextInt()));
+        }
+        System.out.println("original stack:" + originalStack);
+        Stack<Integer> orderedStack = sortStack(originalStack);
+        System.out.println("ordered stack:" + orderedStack);
+    }
+
+    private static Stack<Integer> sortStack(Stack<Integer> originalStack) {
+        Stack<Integer> tempStack = new Stack<>();
+        while (true) {
+            if (originalStack.isEmpty()) {
+                moveAllItemToAnother(tempStack, originalStack);
+                return originalStack;
+            }
+            if (tempStack.isEmpty()) {
+                tempStack.push(originalStack.pop());
+                continue;
+            }
+            int current = originalStack.peek();
+            int top = tempStack.peek();
+            if (current < top) {
+                tempStack.push(current);
+            } else {
+                moveAllItemToAnother(tempStack, originalStack);
+
+            }
+        }
+    }
+
+    private static void moveAllItemToAnother(Stack<Integer> fromStack, Stack<Integer> toStack) {
+        while (true) {
+            if (fromStack.isEmpty()) {
+                return;
+            }
+            toStack.push(fromStack.pop());
+        }
+    }
+}

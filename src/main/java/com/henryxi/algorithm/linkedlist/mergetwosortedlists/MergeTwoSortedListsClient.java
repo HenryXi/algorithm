03009a70c33e21ab410c0346227f12eb1247b910
yong <xxy668@foxmail.com>
2020-09-07 08:47:24 +0800

    auto commit

--- src/main/java/com/henryxi/algorithm/linkedlist/mergetwosortedlists/MergeTwoSortedListsClient.java
+++ src/main/java/com/henryxi/algorithm/linkedlist/mergetwosortedlists/MergeTwoSortedListsClient.java
@@ -39,9 +39,27 @@ public class MergeTwoSortedListsClient {
     }
 
     public static ListNode myMergeTwoLists(ListNode l1, ListNode l2) {
-
+        ListNode res = new ListNode(0);
+        ListNode cursor=res;
+        while (l1!=null && l2!=null){
+            if(l1.val>l2.val){
+                cursor.next=l2;
+                l2=l2.next;
+                cursor=cursor.next;
+            }else{
+                cursor.next=l1;
+                l1=l1.next;
+                cursor=cursor.next;
+            }
+        }
+        if(l1==null){
+            cursor.next=l2;
+        }else{
+            cursor.next=l1;
+        }
+        return res.next;
     }
-
+}
 class ListNode {
     int val;
     ListNode next;

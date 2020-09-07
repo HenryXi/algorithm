5d52b81192e6ce7ec334882019da622061b82772
yong <xxy668@foxmail.com>
2020-09-07 08:48:50 +0800

    auto commit

---
+++ src/main/java/com/henryxi/algorithm/linkedlist/medianoftwosortedarrays/MedianOfTwoSortedArraysClient.java
@@ -0,0 +1,10 @@
+package com.henryxi.algorithm.linkedlist.medianoftwosortedarrays;
+
+/**
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ */
+public class MedianOfTwoSortedArraysClient {
+    public static void main(String[] args) {
+
+    }
+}

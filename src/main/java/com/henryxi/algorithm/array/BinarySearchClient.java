8c18ad9b070b3e65896559b34680302174fe8372
yong <xxy668@foxmail.com>
2020-09-22 15:33:09 +0800

    auto commit

---
+++ src/main/java/com/henryxi/algorithm/array/BinarySearchClient.java
@@ -0,0 +1,25 @@
+package com.henryxi.algorithm.array;
+
+public class BinarySearchClient {
+    public static void main(String[] args) {
+        int[] a = new int[]{2,3,4,4,4,7,7,8,10,10,11,12,13,14,15,15,17,18,19,23,24,24,24,24,25,26,26,26,27,27,28,29,29,30,33,36,38,38,40,40,41,43,43,43,44,46,46,47,51,52,52,53,54,56,57,57,57,58,58,61,61,61,62,64,64,66,66,67,67,67,70,72,74,74,74,75,75,78,78,78,79,79,80,83,83,83,83,84,84,86,88,89,89,90,91,91,92,93,93,96};
+        int res = search(100, 1, a);
+        System.out.println(res);
+    }
+
+    public static int search(int n, int v, int[] a) {
+        int start = 0;
+        int end = n -1;
+        int temp=n;
+        while(start < end){
+            int mid = (end +start)/2;
+            if(a[mid]<v){
+                start = mid+1;
+            }else if(a[mid]>=v){
+                temp= mid;
+                end= mid;
+            }
+        }
+        return temp +1 ;
+    }
+}
---
+++ src/main/java/com/henryxi/algorithm/tree/binarytreezigzaglevelordertraversal/BinaryTreeZigzagLevelOrderTraversalClient.java
@@ -0,0 +1,39 @@
+package com.henryxi.algorithm.tree.binarytreezigzaglevelordertraversal;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeZigzagLevelOrderTraversalClient {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.pop();
+        }
+        return null;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

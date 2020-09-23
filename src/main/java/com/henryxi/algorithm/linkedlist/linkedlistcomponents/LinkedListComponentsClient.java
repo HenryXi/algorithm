32a589f8bdf19f8406957fde9d916692a344eff7
yong <xxy668@foxmail.com>
2020-09-23 20:07:20 +0800

    auto commit

---
+++ src/main/java/com/henryxi/algorithm/linkedlist/linkedlistcomponents/LinkedListComponentsClient.java
@@ -0,0 +1,31 @@
+package com.henryxi.algorithm.linkedlist.linkedlistcomponents;
+
+/**
+ * https://leetcode.com/problems/linked-list-components/
+ */
+public class LinkedListComponentsClient {
+    public static void main(String[] args) {
+
+    }
+
+    public int numComponents(ListNode head, int[] G) {
+        return 0;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}

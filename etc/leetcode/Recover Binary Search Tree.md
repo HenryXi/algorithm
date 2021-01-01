d91b5bbac909b54bc691604ee415cabe080cd135
xixiaoyong <xixiaoyong@xiaomi.com>
2021-01-01 23:46:42 +0800

    this is auto commit

---
+++ etc/leetcode/Recover Binary Search Tree.md
@@ -0,0 +1,15 @@
+# Recover Binary Search Tree
+[Problem Description](https://leetcode.com/problems/recover-binary-search-tree/)
+
+```
+
+```
+
+| Runtime       | Memory     | 
+| :------------- | :---------- |
+|  ms | 5 MB	   |
+
+
+[henryxi leetcode list](http://www.henryxi.com/leetcode)
+
+EOF
--- src/main/java/com/henryxi/algorithm/tree/recoverbinarysearchtree/RecoverBinarySearchTreeClient.java
+++ src/main/java/com/henryxi/algorithm/tree/recoverbinarysearchtree/RecoverBinarySearchTreeClient.java
@@ -1,13 +1,51 @@
 package com.henryxi.algorithm.tree.recoverbinarysearchtree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RecoverBinarySearchTreeClient {
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode(1, new TreeNode(3, null,new TreeNode(2)), null);
+        recoverTree(root);
+        printTree(root);
     }
 
-//    todo not finish
-    public void recoverTree(TreeNode root) {
+    private static void printTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.remove();
+            System.out.println(curr == null ? null : curr.val);
+            if (curr == null) {
+                continue;
+            }
+            if (curr.left == null && curr.right == null) {
+                continue;
+            }
+            queue.add(curr.left);
+            queue.add(curr.right);
+        }
+    }
 
+    //todo not finish
+    static TreeNode t1, t2, pre;
+    public static void recoverTree(TreeNode root) {
+        inorder(root);
+        int temp = t1.val;
+        t1.val = t2.val;
+        t2.val = temp;
+    }
+    public static void inorder(TreeNode root){
+        if (root == null) return ;
+        inorder(root.left);
+        if (pre != null && pre.val > root.val) {
+            if (t1 == null) {
+                t1 = pre;
+            }
+            t2 = root;
+        }
+        pre = root;
+        inorder(root.right);
     }
 }
 

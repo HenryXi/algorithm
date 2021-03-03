cd9a61aebeec6812b7ed1d4fa14bac2bfd200946
xixiaoyong <xixiaoyong@xiaomi.com>
2021-03-03 16:15:59 +0800

    this is auto commit

--- etc/leetcode/3Sum.md
+++ etc/leetcode/3Sum.md
@@ -2,12 +2,47 @@
 [Problem Description](https://leetcode.com/problems/3sum/)
 
 ```
-
+public static List<List<Integer>> threeSum(int[] nums) {
+    int len = nums.length;
+    if (len < 3) {
+        return new ArrayList<>();
+    }
+    if (len == 3 && nums[0] == 0 && nums[1] == 0 && nums[2] == 0) {
+        return Collections.singletonList(Arrays.asList(0, 0, 0));
+    }
+    Arrays.sort(nums);
+    List<List<Integer>> ans = new ArrayList<>();
+    for (int i = 0; i < len; i++) {
+        if (nums[i] > 0) {
+            break;
+        }
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue; // 去重
+        }
+        int L = i + 1;
+        int R = len - 1;
+        while (L < R) {
+            int sum = nums[i] + nums[L] + nums[R];
+            if (sum == 0) {
+                ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+                L++;
+                R--;
+            } else if (sum < 0) {
+                L++;
+            } else {
+                R--;
+            }
+        }
+    }
+    return ans;
+}
 ```
 
 | Runtime       | Memory     | 
 | :------------- | :---------- |
-|  ms | 5 MB	   |
+|  19 ms | 42.5 MB	   |
 
 
 [henryxi leetcode list](http://www.henryxi.com/leetcode)
--- src/main/java/com/henryxi/algorithm/array/threesum/ThreeSumClient.java
+++ src/main/java/com/henryxi/algorithm/array/threesum/ThreeSumClient.java
@@ -1,25 +1,53 @@
 package com.henryxi.algorithm.array.threesum;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
-//TODO not finish
+
 public class ThreeSumClient {
     public static void main(String[] args) {
-        Integer[] nums = new Integer[]{-1, 0, 1, 2, -1, -4};
-        Arrays.sort(nums);
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         List<List<Integer>> lists = threeSum(nums);
         System.out.println(lists);
+
     }
 
-    public static List<List<Integer>> threeSum(Integer[] nums) {
-        List<List<Integer>> list = new LinkedList<>();
-        int head = 0;
-        int tail = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int len = nums.length;
+        if (len < 3) {
+            return new ArrayList<>();
         }
-        return list;
+        if (len == 3 && nums[0] == 0 && nums[1] == 0 && nums[2] == 0) {
+            return Collections.singletonList(Arrays.asList(0, 0, 0));
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // 去重
+            }
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+                    L++;
+                    R--;
+                } else if (sum < 0) {
+                    L++;
+                } else {
+                    R--;
+                }
+            }
+        }
+        return ans;
     }
 }

09e2a9b7b9c25c9111c59e567193f7199b9475d2
xixiaoyong <xixiaoyong@xiaomi.com>
2021-03-03 20:36:35 +0800

    this is auto commit

---
+++ src/main/java/com/henryxi/algorithm/array/foursum/FourSumClient.java
@@ -0,0 +1,21 @@
+package com.henryxi.algorithm.array.foursum;
+
+import java.util.ArrayList;
+import java.util.List;
+//todo not finish
+public class FourSumClient {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int length = nums.length;
+        if(length<3){
+            return new ArrayList<>();
+        }
+    }
+}

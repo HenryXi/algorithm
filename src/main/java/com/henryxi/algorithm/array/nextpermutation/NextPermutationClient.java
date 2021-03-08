946d2fead3a21c23fe322caadbd475cf97f6c8f3
xixiaoyong <xixiaoyong@xiaomi.com>
2021-03-08 11:44:28 +0800

    this is auto commit

---
+++ src/main/java/com/henryxi/algorithm/array/nextpermutation/NextPermutationClient.java
@@ -0,0 +1,22 @@
+package com.henryxi.algorithm.array.nextpermutation;
+
+
+/**
+ * 题干的意思是：找出这个数组排序出的所有数中，刚好比当前数大的那个数
+ * <p>
+ * 比如当前 nums = [1,2,3]。这个数是123，找出1，2，3这3个数字排序可能的所有数，排序后，比123大的那个数 也就是132
+ * <p>
+ * 如果当前 nums = [3,2,1]。这就是1，2，3所有排序中最大的那个数，那么就返回1，2，3排序后所有数中最小的那个，也就是1，2，3 -> [1,2,3]
+ */
+//todo not finish
+public class NextPermutationClient {
+    public static void main(String[] args) {
+
+    }
+
+    public void nextPermutation(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+        }
+    }
+}

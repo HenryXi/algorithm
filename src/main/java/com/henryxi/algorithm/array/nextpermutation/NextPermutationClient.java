077057289141fec6ffe35bcf6a721c14e8957518
xixiaoyong <xixiaoyong@xiaomi.com>
2021-03-08 20:09:19 +0800

    this is auto commit

--- src/main/java/com/henryxi/algorithm/array/nextpermutation/NextPermutationClient.java
+++ src/main/java/com/henryxi/algorithm/array/nextpermutation/NextPermutationClient.java
@@ -1,6 +1,8 @@
 package com.henryxi.algorithm.array.nextpermutation;
 
 
+import java.util.Arrays;
+
 /**
  * 题干的意思是：找出这个数组排序出的所有数中，刚好比当前数大的那个数
  * <p>
@@ -11,12 +13,39 @@ package com.henryxi.algorithm.array.nextpermutation;
 //todo not finish
 public class NextPermutationClient {
     public static void main(String[] args) {
-
+        int[] nums = new int[]{1, 2, 3};
+        nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
-    public void nextPermutation(int[] nums) {
-        for (int i = nums.length - 1; i >= 0; i--) {
-
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
         }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }

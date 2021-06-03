44d25e650cef6d501bd868088aee54a3582a4be1
yong <xxy668@foxmail.com>
2021-06-03 08:53:25 +0800

    auto commit

---
+++ src/main/java/com/henryxi/algorithm/backtracking/PermutationsClient.java
@@ -0,0 +1,13 @@
+package com.henryxi.algorithm.backtracking;
+
+import java.util.List;
+//todo not finish
+public class PermutationsClient {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+
+    }
+}

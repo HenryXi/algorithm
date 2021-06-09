004d61d5ced66535efca99d2291616066b69bd72
yong <xxy668@foxmail.com>
2021-06-09 08:35:55 +0800

    auto commit

--- src/main/java/com/henryxi/algorithm/string/zigzagconversion/ZigZagConversionClient.java
+++ src/main/java/com/henryxi/algorithm/string/zigzagconversion/ZigZagConversionClient.java
@@ -3,10 +3,30 @@ package com.henryxi.algorithm.string.zigzagconversion;
 //todo not finish
 public class ZigZagConversionClient {
     public static void main(String[] args) {
-
+        ZigZagConversionClient client = new ZigZagConversionClient();
+        client.convert("PAYPALISHIRING", 4);
     }
 
     public String convert(String s, int numRows) {
+        int length = s.length();
+        int arraysNum = length / (2 * numRows - 2) + 1;
+        int[][] temp = new int[arraysNum][numRows];
+        for (int i = 0; i < arraysNum; i++) {
+            for (int j = 0; j < numRows; j++) {
+                int modeRows = i % (numRows-1);
+                if(modeRows==0){
+                    temp[i][j] = i*numRows+
+                }else{
 
+                }
+                temp[i][j] = -1;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            int modCycle = i % (2 * numRows - 2);
+            int modRows = i % numRows;
+            s.charAt()
+        }
     }
 }
